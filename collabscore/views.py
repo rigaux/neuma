8a2c812987fc07f2c0cea32928fca47dfe85a26d
philippe <rigaux.phil@gmail.com>
2022-04-01 19:22:56 +0200

    Implementation of OMR parser

--- collabscore/views.py
+++ collabscore/views.py
@@ -1,5 +1,5 @@
 
-import os, datetime, math
+import os
 
 from django.shortcuts import render
 from manager.models import (
@@ -8,19 +8,31 @@ from manager.models import (
 )
 from django.conf import settings
 
-import json
+from lib.collabscore.parser import *
+
 import jsonref
+import json
 from pprint import pprint
 from jsonref import JsonRef
-import jsonschema 
 
 def tests(request):
 	context = {"titre": "Tests Philippe"}
 
+	# Where is the schema?
 	data_dir = os.path.join(settings.BASE_DIR, 'static', 'json')
+	schema_file = os.path.join(data_dir, 'dmos_schema.json')
+	base_uri='file://' + data_dir + '/'
+	
+	# Parse the schema
+	try:
+		parser = CollabScoreParser(schema_file, base_uri)
+		context['message_schema'] = "Le schéma est valide"
+	except jsonschema.SchemaError as ex:
+		context['message_schema'] = "Schema parsing error: " + str (ex)
+	except Exception:
+		"Unexpected error during parser initialization: " + str (ex)
 
-	# Load the schema
-	sch_file = open(os.path.join(data_dir, 'dmos_schema.json'))
+	''' Load the schema
 	schema  = json.load(sch_file)
 	
 	resolver = jsonschema.RefResolver(referrer=schema, 
@@ -33,28 +45,28 @@ def tests(request):
 		context['message_schema'] = "Le schéma est valide"
 	except jsonschema.SchemaError as ex:
 		context['message_schema'] = str (ex)
-
+	'''
 	
-	# Load the data
-	#data_file = open()
+	# Load the data and resolve references
+	data = jsonref.load_uri('http://collabscore.org/dmos/data/dmos_data.json', 
+						base_uri='http://collabscore.org/dmos/data/')
 	
-	data = jsonref.load_uri('http://collabscore.org/dmos/data/dmos_data.json', base_uri='http://collabscore.org/dmos/data/')
-	
-	# Ecrivons le document complet dans un fichier
+	# Write the full document in a local file
 	with open(os.path.join(data_dir, 'full_example.json'), 'a') as the_file:
 		json.dump(data, the_file)
 		
 	context["document"] = data
 	
 	try:
-		jsonschema.validate (instance=data, schema=schema, resolver=resolver)
+		parser.validate_data (data)
 		context['message'] = "Validation effectuée"
-	except jsonschema.ValidationError as ex:
-		errors = sorted(validator.iter_errors(data), key=lambda e: e.path)
-		context['errors'] = errors
-		context['message'] = "Erreur de validation données"
-	except jsonschema.SchemaError as ex:
+	except Exception as ex:
 		context['message'] = str(ex)
-		
+	
+	# OK now build the internal score representation from the JSON content
+	omr_score = OmrScore (data)
+	
+	context["omr_score"] = omr_score
+	
 	return render(request, 'collabscore/tests.html', context)
 
