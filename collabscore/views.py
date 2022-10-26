4dde339f03e2427310e8913097cbdeeddf3a6be7
philippe <rigaux.phil@gmail.com>
2022-10-26 18:34:49 +0200

    Service to create a source

--- collabscore/views.py
+++ collabscore/views.py
@@ -24,7 +24,8 @@ def index(request):
 	context["corpus"] = Corpus.objects.get(ref=settings.NEUMA_COLLABSCORE_CORPUS_REF)
 	context["opus_list"] = []
 	
-	for opus in Opus.objects.filter(corpus=context["corpus"]):
+	#for opus in Opus.objects.filter(corpus=context["corpus"]):
+	for opus in Opus.objects.filter(ref__startswith=settings.NEUMA_COLLABSCORE_CORPUS_REF):
 		if opus.mei:
 			url_mei = request.build_absolute_uri(opus.mei.url)
 		else:
@@ -44,7 +45,7 @@ def index(request):
 	return render(request, 'collabscore/index.html', context)
 
 
-def tests(request, test_sample, opus_ref):
+def tests(request, opus_ref):
 	context = {"titre": "Tests Philippe"}
 
 	opus = Opus.objects.get(ref=opus_ref)
@@ -80,34 +81,13 @@ def tests(request, test_sample, opus_ref):
 		context['message_schema'] = "Unexpected error during parser initialization: " + str (ex)
 		return render(request, 'collabscore/tests.html', context)
 
-	# Example to process
-	print ("Test sample " + test_sample)
-	if test_sample == 1:
-		input_file = "dmos_ex1.json"
-		# Load the data and resolve references	
-		data_dir = os.path.join(dmos_dir, 'data/ex1/')
-		file_url =  data_dir + input_file
-	else:
-		input_file = "dmos.json"
-		data_dir = os.path.join(dmos_dir, 'data/ex1/')
-		file_url =  url_dmos
-		
-	# Get the file name without extension
-	input_file_root = os.path.splitext(os.path.basename(input_file))[0]
-
-	#print (f'Load {file_url} from {data_dir}')
 	try:
-		data = jsonref.load_uri(file_url,base_uri=data_dir)
+		data = jsonref.load_uri(url_dmos,base_uri="")
 	except Exception as ex:
 		context['message'] = "Data loading error: " + str(ex)
 		return render(request, 'collabscore/tests.html', context)
 
 	#return render(request, 'collabscore/tests.html', context)
-
-	# Write the full document in a local file
-	#json_dir = os.path.join(settings.BASE_DIR, 'static', 'json')
-	#with open(os.path.join(json_dir, 'full_example.json'), 'a') as the_file:
-	#	json.dump(data, the_file)
 	
 	try:
 		parser.validate_data (data)
@@ -115,16 +95,6 @@ def tests(request, test_sample, opus_ref):
 	except Exception as ex:
 		context['message'] = "Validation error : " + str(ex)
 	
-	
-	# Build the encoded score
-	corpus = Corpus.objects.get(ref=settings.NEUMA_COLLABSCORE_CORPUS_REF)
-	full_opus_ref = corpus.ref + settings.NEUMA_ID_SEPARATOR + input_file_root
-	try:
-		opus = Opus.objects.get(ref=full_opus_ref)
-	except Opus.DoesNotExist as e:
-		# Create the Opus
-		opus = Opus(corpus=corpus, ref=full_opus_ref, title="Test collabscore")
-		opus.save()
 		
 	# OK now build the internal score representation from the JSON content
 	my_url = request.build_absolute_uri("/")[:-1] 
