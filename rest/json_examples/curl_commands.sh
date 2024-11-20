c5520a9802e5e94949022c987cd6348153cc6bb1
Philippe RIGAUX <philippe.rigaux@cnam.fr>
2024-11-20 07:24:37 +0100

    Ajustement editions

--- rest/json_examples/curl_commands.sh
+++ rest/json_examples/curl_commands.sh
@@ -1,14 +1,17 @@
-curl -u rigaux:Fuf3a3wu! -X POST http://localhost:8000/rest/collections/all:collabscore:saintsaens-ref:C006_0/_sources/iiif/_apply_editions/ \
+curl -u collabscore:collabscore -X POST http://localhost:8000/rest/collections/all:collabscore:saintsaens-ref:C006_0/_sources/iiif/_apply_editions/ \
 -d @eds_replace_clef.json  -H "Content-Type: application/json"
 
 curl -u sameh:collabscore -X POST http://neuma.huma-num.fr/rest/collections/all:collabscore:saintsaens-ref:C006_0/_sources/iiif/_apply_editions/ \
 -d @replace_clef.json  -H "Content-Type: application/json"
 
-curl -u rigaux:fuf3a3wu -X POST http://neuma.huma-num.fr/rest/collections/all:collabscore:saintsaens-ref:C006_0/_sources/iiif/_apply_editions/ \
+curl -u collabscore:collabscore -X POST http://neuma.huma-num.fr/rest/collections/all:collabscore:saintsaens-ref:C006_0/_sources/iiif/_apply_editions/ \
 -d @replace_clef.json  -H "Content-Type: application/json" > t.xml
 
-curl -u rigaux:Fuf3a3wu! -X POST http://localhost:8000/rest/collections/all%3Acollabscore%3Asaintsaens-ref%3AC006_0/_sources/iiif/_editions/ \
--d @eds_replace_clef.json  -H "Content-Type: application/json"
+curl -u collabscore:collabscore -X POST http://localhost:8000/rest/collections/all%3Acollabscore%3Asaintsaens-ref%3AC006_0/_sources/iiif/_editions/ \
+-d @eds_replace_clef.json  -H "Content-Type: application/json" > t.xml
 
+curl -u collabscore:collabscore -X POST http://localhost:8000/rest/collections/all%3Acollabscore%3Asaintsaens-ref%3AC006_0/_sources/iiif/_apply_editions/ \
+-d @replace_clef.json  -H "Content-Type: application/json" > t.xml
 
-Le type frozen est-il obligatoire ?
+curl -u collabscore:collabscore -X POST http://localhost:8000/rest/collections/all%3Acollabscore%3Asaintsaens-ref%3AC006_0/_sources/iiif/_apply_editions/ \
+-d @replace_timesign.json  -H "Content-Type: application/json" > t.xml
